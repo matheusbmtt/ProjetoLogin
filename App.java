894c17d0c25534bfdfc46ef85447885095dc80a2
Matheus Mattos <matheusbmtt@gmail.com>
2025-03-07 21:27:03 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,22 @@
+package projetoLogin;
+
+import java.sql.Connection;
+
+public class App {
+    public static void main(String[] args) {
+        ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+        Connection conexao = conexaoSQLite.conectar();
+        
+        // Criar tabela
+       CriarTabela ct = new CriarTabela();
+       ct.criarTabelaUsuarios(conexao);
+       
+       InserirUsuario.inserirUsuario(conexao, "Leo", "Senha" );
+       
+      
+       
+       
+    }
+        
+        
+}
